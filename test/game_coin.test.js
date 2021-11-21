4da165130aab1cf0a26f2784817af92db11d77fb
shreeya13garg <51780388+shreeya13garg@users.noreply.github.com>
2021-11-22 03:15:09 +0530

    Completed Tests

--- test/game_coin.test.js
+++ test/game_coin.test.js
@@ -1,79 +1,185 @@
-const { assert } = require('chai');
+const { assert } = require("chai");
 
 const Token = artifacts.require("Token");
 
-require('chai')
-.use(require('chai-as-promised'))
-.should()
+require("chai").use(require("chai-as-promised")).should();
+
+contract("Token BuyTest", async ([owner, player1, player2]) => {
+	it("Buying Tokens for Multi Player and Checking Test", async () => {
+		const tokenInstance = await Token.deployed();
+		const owner_balance = await tokenInstance.getBalance();
+		const player1_balance = await tokenInstance.getBalanceOther(player1);
+		const player2_balance = await tokenInstance.getBalanceOther(player2);
+		//player2 = 0xF033fC3c3daf5baEDAd2845Ee8C47F007547e062
+		// player1 = 0x900D340f7ba5F95b28aC66DebdFEc495b48a7C4B
+		// Owner = 0x0A01b9fF422300177D54D4C09bf3961Eb2B417F2
+
+		//const contractAddress = await tokenInstance.getContractAddress();
+
+        console.log(
+			player1_balance.toString(),
+			player2_balance.toString()
+		);
+
+		await tokenInstance.buyTokens(100, { from: player1, value: 1000 });
+		await tokenInstance.buyTokens(250, { from: player2, value: 1000 });
+
+		let player1_balance_updated = await tokenInstance.getBalanceOther(player1);
+		let player2_balance_updated = await tokenInstance.getBalanceOther(player2);
+        console.log("Added Money to Player 1 and Player 2")
+		console.log(
+			player1_balance_updated.toString(),
+			player2_balance_updated.toString()
+		);
+        assert.equal((player1_balance_updated-player1_balance).toString(),100)
+        assert.equal((player2_balance_updated-player2_balance).toString(),250)
+
+		const owner_balance_updated = await tokenInstance.getBalanceOther(owner);
+		const etherAmount = await tokenInstance.getContractBalance();
+		assert.equal(etherAmount.toString(),2000)
+
+		
+
+		console.log("The Ether Amount for buying the tokens " + etherAmount);
+		assert.equal(
+			(player2_balance_updated - -player1_balance_updated).toString(),
+			(owner_balance - owner_balance_updated).toString()
+		);
 
 
-contract('Token',([owner,msg,pot,clientId,viaEvent])=> {
+	});
 
-    describe('Token Deployment',async() =>{
-        it('checking contracts balance of owner',async()=>
-        {
-            let token = await Token.new()
-            let original_balance = await token.balanceOf(owner)
-            token.buyTokens(1000000)
-        .then(async (token,original_balance)=> 
-        {
-            let new_balance = await token.balanceOf(owner);
-            console.log("Printing Original Balance")
-            console.log(original_balance.toString())
+	it("Starting Game and Testing Round", async () => {
+		const tokenInstance = await Token.deployed();
 
-            console.log("Printing New Balance")
-            console.log(new_balance.toString())
-            //assert.equal(100,100)
-            assert.equal(new_balance.toString(),(original_balance-1000000).toString())
-            //assert.equal(msg_new_balance.toString(),(msg_old_balance+1000000).toString())
-        })
-    })
-
-    //Showing some stupid error(balanceOf)
-    it('checking contracts balance of senders',async()=>
-    {
-        let token = await Token.new()
-        let msg_old_balance=await token.balanceOf(msg.sender);
-        token.buyTokens(1000000)
-    .then(async (token,msg_old_balance)=> 
-    {
-        let msg_new_balance=await token.balanceOf(msg.sender);
-        console.log("Printing Original Balance-Sender")
-        console.log(msg_old_balance.toString())
-
-        console.log("Printing New Balance-Senders")
-        console.log(msg_new_balance.toString())
+        const owner_balance = await tokenInstance.getBalance();
+        console.log("Owner Balance")
+        console.log(owner_balance.toString());
         
-        assert.equal(msg_new_balance.toString(),(msg_old_balance+1000000).toString())
-    })
-    })
+		const player1_balance = await tokenInstance.getBalanceOther(player1);
+		const player2_balance = await tokenInstance.getBalanceOther(player2);
+        console.log("Printing Initial Balance")
+		console.log(player1_balance.toString(), player2_balance.toString());
+		const room_id = "ohRgclC";
+		const p1_id = "p1hash";
+		const p2_id = "p2hash";
+		await tokenInstance.transfer(room_id, 2, p1_id, "bet", { from: player1 });
+		await tokenInstance.transfer(room_id, 2, p2_id, "bet", { from: player2 });
+		await tokenInstance.transfer(room_id, 4, p1_id, "raise", {
+			from: player1,
+		});
+		await tokenInstance.transfer(room_id, 4, p2_id, "bet", {
+			from: player2,
+		});
+		await tokenInstance.transfer(room_id, 4, p1_id, "show", {
+			from: player1,
+		});
+        console.log("Player 1 spends 10 to pot")
+        console.log("Player 2 spends 6 to pot")
+		const pot_balance=await tokenInstance.getBalancePot(room_id);
+		console.log("Priting Pot Balance")
+		console.log(pot_balance.toString())
+		assert.equal(pot_balance.toString(),16)
 
+        const owner_balance_updated = await tokenInstance.getBalance();
 
-    /*
-
-    it('checking Transfer function',async()=>
-    {
-        let token = await Token.new()
-        let msg_old_balance=await token.balanceOfPot(pot);
-        let result=await token.transfer(pot,1000000,clientId,viaEvent)
-    .then(async (token,msg_old_balance)=> 
-    {
-        let msg_new_balance=await token.balanceOfPot(pot);
-        console.log("Printing Original Balance-Sender")
-        console.log(msg_old_balance.toString())
-
-        console.log("Printing New Balance-Senders")
-        console.log(msg_new_balance.toString())
         
-        assert.equal(msg_new_balance.toString(),(msg_old_balance+1000000).toString())
-    })
-    })
+        
+		player1_balance_updated = await tokenInstance.getBalanceOther(player1);
+		player2_balance_updated = await tokenInstance.getBalanceOther(player2);
 
-    */
+        console.log("Printing Final Balance")
+		console.log(
+			player1_balance_updated.toString(),
+			player2_balance_updated.toString()
+		);
+        assert.equal((player1_balance-player1_balance_updated).toString(),10)
+        assert.equal((player2_balance-player2_balance_updated).toString(),6)
 
+        await tokenInstance.winnerTransfer(room_id, player1),
+			{
+				from: owner,
+			};
 
-    
-    })
+		const pot_balance_updated=await tokenInstance.getBalancePot(room_id);
+		console.log("Printing Updated Pot Balance")
+		console.log(pot_balance_updated.toString())
+		assert.equal(pot_balance_updated.toString(),0)
 
+        
+        console.log("Printing Final Balance of player 1")
+        player1_balance_final_updated = await tokenInstance.getBalanceOther(player1);
+        console.log(player1_balance_final_updated.toString());
+        assert.equal((player1_balance_final_updated-player1_balance_updated).toString(),16)
 
-})
+        
+	});
+
+	it("Testing Winner Transfer Tie", async () => {
+		const tokenInstance = await Token.deployed();
+        
+		const player1_balance = await tokenInstance.getBalanceOther(player1);
+		const player2_balance = await tokenInstance.getBalanceOther(player2);
+        console.log("Printing Initial Balance")
+		console.log(player1_balance.toString(), player2_balance.toString());
+		const room_id = "ohRgclC";
+		const p1_id = "p1hash";
+		const p2_id = "p2hash";
+		await tokenInstance.transfer(room_id, 2, p1_id, "bet", { from: player1 });
+		await tokenInstance.transfer(room_id, 2, p2_id, "bet", { from: player2 });
+		await tokenInstance.transfer(room_id, 4, p1_id, "raise", {
+			from: player1,
+		});
+		await tokenInstance.transfer(room_id, 4, p2_id, "bet", {
+			from: player2,
+		});
+		await tokenInstance.transfer(room_id, 8, p1_id, "raise", {
+			from: player1,
+		});
+		await tokenInstance.transfer(room_id, 8, p2_id, "show", {
+			from: player2,
+		});
+        console.log("Player 1 spends 14 to pot")
+        console.log("Player 2 spends 14 to pot")
+
+		const pot_balance=await tokenInstance.getBalancePot(room_id);
+		console.log("Priting Pot Balance")
+		console.log(pot_balance.toString())
+
+		assert.equal(pot_balance.toString(),28)
+
+        
+		player1_balance_updated = await tokenInstance.getBalanceOther(player1);
+		player2_balance_updated = await tokenInstance.getBalanceOther(player2);
+
+        console.log("Printing Final Balance")
+		console.log(
+			player1_balance_updated.toString(),
+			player2_balance_updated.toString()
+		);
+        assert.equal((player1_balance-player1_balance_updated).toString(),14)
+        assert.equal((player2_balance-player2_balance_updated).toString(),14)
+
+        await tokenInstance.winnerTransferTie(room_id, player1,player2),
+			{
+				from: owner,
+			};
+
+		const pot_balance_updated=await tokenInstance.getBalancePot(room_id);
+		console.log("Printing Updated Pot Balance")
+		console.log(pot_balance_updated.toString())
+		assert.equal(pot_balance_updated.toString(),0)
+
+        
+        console.log("Printing Final Balance of player 1")
+        player1_balance_final_updated = await tokenInstance.getBalanceOther(player1);
+        console.log(player1_balance_final_updated.toString());
+        assert.equal((player1_balance_final_updated-player1_balance_updated).toString(),14)
+
+		console.log("Printing Final Balance of player 2")
+        player2_balance_final_updated = await tokenInstance.getBalanceOther(player2);
+        console.log(player2_balance_final_updated.toString());
+        assert.equal((player2_balance_final_updated-player2_balance_updated).toString(),14)
+        
+	});
+});
